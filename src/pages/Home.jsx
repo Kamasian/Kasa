9e869033de74f9e8c8bfacbefc74a72436ffae6c
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-06-27 22:10:41 +0200

    Modification des 4 pages, header, et footer. Ajout des composants Banner et Card

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Banner from "../components/Banner"
+import Card from "../components/Card"
+
+export default function Home() {
+    return (
+        <div>
+            <Banner />
+            <Card />
+        </div>
+    )
+}
