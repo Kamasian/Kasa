fa1b797f124412fb3dc27b3b7f09cd9cc0e10406
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-06-27 23:29:34 +0200

    Modification du header, ajout du scss header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
 import React from "react"
+import logo from "../assets/logo.png"
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <img src={logo} alt="Logo" />
+            <ul>
+                <li>
+                    <Link to={"/"}>Accueil</Link>
+                </li>
+                <li>
+                    <Link to={"/a-propos"}>A Propos</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
