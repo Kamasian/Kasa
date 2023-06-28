d7b957d21d4a43f1184210ad005df6327592ca27
Dugrais Emmanuel <emmanuel.dugrais@gmail.com>
2023-06-28 02:01:28 +0200

    Modification du composant Banner, Ajout du scss Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,15 @@
 import React from "react"
+import { useLocation } from "react-router-dom"
+import bannerHome from "../assets/home_pic.jpg"
+import bannerAbout from "../assets/about_pic.jpg"
 
 export default function Banner() {
+    const location = useLocation()
+
     return (
-        <div>
-            
-        </div>
+        <section className="banner">
+            <img src={location.pathname === "/" ? bannerHome : bannerAbout} alt="Bannière" />
+            {location.pathname === "/" ? <h1>Chez vous, partout et ailleurs</h1> : ""}
+        </section>
     )
 }
